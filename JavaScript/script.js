16ec41c37da8c9d53f1fca7bb0fa3d2ebd66f39e
Axel Rodas <arodav5@gmailmiumg.edu.gt>
2026-05-17 16:36:00 -0600

    Primer commit

---
+++ JavaScript/script.js
@@ -0,0 +1,22 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+topBtn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
